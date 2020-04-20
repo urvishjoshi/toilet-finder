cadcc4df9bc8b7d563e91eb6702e6d978116e6d4
Kishan Gadoya <kishan.gadoya.90@gmail.com>
2020-04-20 17:32:55 +0530

    Kishan commited...

--- resources/views/admin/toiletowner.blade.php
+++ resources/views/admin/toiletowner.blade.php
@@ -1,4 +1,33 @@
 @section('title','Toiletowner')
 @extends('admin.layouts.app')
 @section('toiletowner')
+
+
+	<section>
+		<div class="container">
+			<div class="row">
+				<table class="table table-striped">
+					<thead>
+						<tr>
+							<th>Id</th>
+							<th>E-mail</th>
+							<th>Toilet Address</th>
+							<th>Contact</th>
+							<th>Rattings</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+					    <tr>
+					      <th scope="row">1</th>
+					      <td>abc@gmail.com</td>
+					      <td>Ghare</td>
+					      <td>1234567890</td>
+					      <td>1234567890</td>
+					      <td>1234567890</td>
+					    </tr>
+				</table>
+			</div>	
+		</div>
+	</section>
 @endsection
